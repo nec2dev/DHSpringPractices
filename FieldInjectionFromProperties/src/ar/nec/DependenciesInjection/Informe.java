d26df4e3e9325b81581cb69d2306b601659432d0
Nahuel <71109200+nec2dev@users.noreply.github.com>
2021-02-18 17:56:29 -0300

    Subiendo DHSpringPractices

---
+++ FieldInjectionFromProperties/src/ar/nec/DependenciesInjection/Informe.java
@@ -0,0 +1,10 @@
+package ar.nec.DependenciesInjection;
+
+public class Informe implements CreaInformes{
+	
+	public String getInforme() {
+		
+		return "Te entrego la presentación del informe";
+	}
+
+}
